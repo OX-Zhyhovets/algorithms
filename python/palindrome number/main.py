4bc8ef23d3b496739a2db6af4ed28a48c5c4f137
Prokaznik <oleksandr10605@gmail.com>
2021-12-05 20:18:28 +0300

    palindrome number

---
+++ python/palindrome number/main.py
@@ -0,0 +1,28 @@
+x = 121
+
+def reverseNumber(number):
+	str_num = str(number)
+	reverse = ""
+
+	i = -1
+
+	while i >= -len(str_num):
+		reverse = reverse + str_num[i]
+		i = i-1
+
+	return int(reverse)
+
+def palindromeNumber(number):
+	if "-" in str(number):
+		print("false")
+
+	else:
+		reverse_number = reverseNumber(number)
+
+		if number == reverse_number:
+			print("true")
+
+		else:
+			print("false")
+
+palindromeNumber(x)
